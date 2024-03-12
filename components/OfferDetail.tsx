db0e3e4a37c6206cbcf929143ca761dfef983697
Josojmf <joso,jmf@gmail.com>
2024-03-12 23:53:21 +0100

    Ultimos Retoques

--- components/OfferDetail.tsx
+++ components/OfferDetail.tsx
@@ -1,18 +1,29 @@
 import { FunctionComponent } from "https://esm.sh/v128/preact@10.19.2/src/index.js";
 import { Offer } from "../types.tsx";
 
-
 export const OfferDetail: FunctionComponent<{ offer: Offer }> = ({ offer }) => {
   return (
     <div className="OfferDetails">
       <h1>{offer.title}</h1>
-      <img className ="Maletin" src={"/Maletin.png"} />
-      <p>{offer.company_name}</p>
-      <p>{offer.location}</p>
-      <p>{offer.remote ? "Remote" : "Onsite"}</p>
-      <div dangerouslySetInnerHTML={{
-  __html: offer.description
-}}/>
+
+      <div className="Company">
+        <img className="Maletin" src={"office.png"} alt={offer.company_name} />
+
+        <p>  {offer.company_name}</p>
+      </div>
+      <div className="Location">
+        <img className="Maletin" src={"/Maletin.png"} />
+        <p>{offer.location}</p>
+      </div>
+
+      <div>
+        <p>{offer.remote ? "& Remote" : " Onsite"}</p>
+      </div>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: offer.description,
+        }}
+      />
       <a href={offer.url}>Apply</a>
     </div>
   );
